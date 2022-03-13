680d2eb2262555694385d255d9f162647ab9bf4e
Piotr Kącki <piotrekacki@gmail.com>
2022-03-13 20:41:03 +0100

    Add method GetCommandById and GetAllCommands

---
+++ WebApiCommander/Controllers/CommandsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebApiCommander.Data;
+using WebApiCommander.Models;
+
+namespace WebApiCommander.Controllers
+{
+    //api/commands 
+    [Route("api/commands")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        private readonly MockCommanderRepo _repository = new MockCommanderRepo();
+        //Get api/commands
+        [HttpGet]
+        public ActionResult <IEnumerable<Command>> GetAllCommands()
+        {
+            var commandItems = _repository.GtAppCommands();
+
+            return Ok(commandItems);
+
+        }
+
+        //Get ap/commands/{id}
+
+        [HttpGet("{id}")]
+        public ActionResult <Command> GetCommandById(int id)
+        {
+            
+            {
+                var commandItem= _repository.GetCommandByID(id);
+                return Ok(commandItem);
+            }
+
+        }
+
+        
+    }
+}
